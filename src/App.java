4b5d142d80f7273e5ae4f796b26ea49b1c4032c1
Lucas Santos <kelytonlucas@gmail.com>
2025-04-30 13:37:32 -0300

    feat: implementação classe aluno especial e esboço main

--- src/App.java
+++ src/App.java
@@ -1,18 +1,139 @@
 import java.util.List;
+import java.util.Scanner;
 
 import entidades.Aluno;
 import repositories.AlunoRepository;
+import repositories.ProfessorRepository;
+import repositories.TurmaRepository;
 
 public class App {
+    public static Scanner sc = new Scanner(System.in);
+    public static AlunoRepository alunoRepository = new AlunoRepository();
+    public static ProfessorRepository professorRepository = new ProfessorRepository();
+    public static TurmaRepository turmaRepository = new TurmaRepository();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        AlunoRepository alunoRepository = new AlunoRepository();
+        System.out.println("Escolha um modo: ");
+        System.out.println("1 - Modo Aluno (Normal e Especial)");
+        System.out.println("2 - Modo Disciplina/Turma");
+        System.out.println("3 - Modo Avaliação/Frequencia");
 
-        List<Aluno> alunos = alunoRepository.getAlunos();
+        int escolha = sc.nextInt();
 
-        for (Aluno aluno : alunos) {
-            System.out.println("Aluno: " + aluno.toString());
+        switch (escolha) {
+            case 1:
+                System.out.println("O que deseja fazer?");
+                System.out.println("1 - Cadastrar Aluno ou editar aluno");
+                System.out.println("2 - Matricular Alunos em disciplinas");
+                System.out.println("3 - Ver alunos cadastrados");
+
+                escolha = sc.nextInt();
+
+                switch (escolha) {
+                    case 1:
+                        System.out.println(
+                                "Deseja matrícular aluno/aluno especial ou editar? (1 para matricular, 2 para editar)");
+                        escolha = sc.nextInt();
+                        if (escolha == 1)
+                            matricularAluno();
+
+                        else if (escolha == 2) {
+
+                        } else {
+                            System.out.println("Numero invalido!");
+                        }
+                        break;
+                    case 2: // fazer depois de criar func de matricular turma
+
+                        break;
+                    case 3:
+                        for (Aluno aluno : alunoRepository.getAlunos()) {
+                            System.out.println("Matricula: " + aluno.getMatricula() + " Nome: " + aluno.getNome()
+                                    + " Curso: " + aluno.getCurso());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case 2:
+                System.out.println("O que deseja fazer?");
+                System.out.println("1 - Cadastrar novo Professor");
+                System.out.println("2 - Cadastrar Disciplina");
+                System.out.println("3 - Criar turmas");
+                System.out.println("4 - Ver Turmas disponíveis");
+
+                escolha = sc.nextInt();
+
+                switch (escolha) {
+                    case 1:
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case 3:
+                System.out.println("O que deseja fazer?");
+                System.out.println("1 - Lançar Notas ou lançar presença");
+                System.out.println("2 - Ver boletim individual");
+                System.out.println("3 - Ver boletim com dados da turma");
+                switch (escolha) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            default:
+                break;
         }
 
+        sc.close();
     }
+
+    public static void matricularAluno() {
+        System.out.println("O aluno que deseja matricular é especial (s ou n)?");
+        char escolha = sc.next().charAt(0);
+
+        if (escolha == 's' || escolha == 'S') {
+            System.out.println("Digite a matrícula do novo aluno especial: ");
+            int matricula = sc.nextInt();
+
+            AlunoEspecial novoAlunoEspecial = new AlunoEspecial();
+
+        } else {
+
+            System.out.println("Digite a matrícula do aluno: ");
+            int matricula = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Digite o nome do Aluno: ");
+            String nome = sc.nextLine();
+
+            System.out.println("Digite o nome do curso: ");
+            String curso = sc.nextLine();
+
+            Aluno aluno = new Aluno(nome, matricula, curso, Boolean.valueOf(false));
+
+            alunoRepository.save(aluno);
+        }
+    }
+
 }
---
+++ src/entidades/AlunoEspecial.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class AlunoEspecial extends Aluno {
+    private Boolean alunoEspecial;
+
+    public AlunoEspecial() {
+        super();
+        this.alunoEspecial = Boolean.valueOf(false);
+    }
+
+    public AlunoEspecial(String nome, Integer matricula, String curso, Boolean trancamento, Boolean alunoEspecial) {
+        super(nome, matricula, curso, trancamento);
+        this.alunoEspecial = Boolean.valueOf(alunoEspecial);
+    }
+
+    public Boolean getAlunoEspecial() {
+        return alunoEspecial;
+    }
+
+    public void setAlunoEspecial(Boolean alunoEspecial) {
+        this.alunoEspecial = alunoEspecial;
+    }
+}
